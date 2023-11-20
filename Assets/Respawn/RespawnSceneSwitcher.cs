a3cb982a41194132f0b6659b2fe33f281348d388
Paweahmad <73645619+Paweahmad@users.noreply.github.com>
2023-11-20 11:44:29 +0100

    sdad

---
+++ Assets/Respawn/RespawnSceneSwitcher.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RespawnSceneSwitcher : MonoBehaviour
+{
+    public string targetSceneName = "Map"; // Specify the target scene name
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Check if the collider is from the player's hand
+        {
+            SceneManager.LoadScene(targetSceneName); // Load the target scene
+        }
+    }
+}
