fc70508dbb4f9ba6e3e9d0fc48ee2df8f03dbc4f
Paweahmad <73645619+Paweahmad@users.noreply.github.com>
2024-03-22 05:41:42 +0100

    SceneSwitcher

---
+++ Assets/SceneTransitionManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public FadeScreen fadeScreen;
+
+    public void GoToScene(int sceneIndex)
+    {
+        StartCoroutine(GoToSceneRoutine(sceneIndex));
+    }
+    IEnumerator GoToSceneRoutine(int sceneIndex)
+    {
+        fadeScreen.FadeOut();
+        yield return new WaitForSeconds(fadeScreen.fadeDuration);
+
+        //Launch new scene
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
