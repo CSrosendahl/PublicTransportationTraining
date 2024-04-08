30f4b6546b1f49757c645a898b338f14a52f28fb
MariaL23 <91759715+MariaL23@users.noreply.github.com>
2024-04-08 13:03:09 +0200

    Bye bye sweet children

---
+++ Assets/Scripts/ChildDisable.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildDisable : MonoBehaviour
+{
+    public Transform parentObject; // Reference to the parent object whose children you want to enable/disable
+    public int childrenToEnable = 2; // Number of children to enable at a time
+    public float enableDelay = 15f; // Delay between enabling children
+    public float disableDelay = 25f; // Delay before disabling children
+    private Transform[] children; // Array to hold references to the children objects
+    private int enableIndex = 1; // Index to keep track of which child to enable next
+    private int disableIndex = 0; // Index to keep track of which child to disable next
+
+    void Start()
+    {
+        // Get references to all children of the parent object
+        children = new Transform[parentObject.childCount];
+        for (int i = 0; i < parentObject.childCount; i++)
+        {
+            children[i] = parentObject.GetChild(i);
+        }
+
+        // Start the coroutine to enable/disable children
+        StartCoroutine(EnableDisableRoutine());
+    }
+
+    IEnumerator EnableDisableRoutine()
+    {
+        while (true)
+        {
+            // Enable the specified number of children
+            for (int i = 0; i < childrenToEnable; i++)
+            {
+                int indexToEnable = (enableIndex + i) % children.Length;
+                children[indexToEnable].gameObject.SetActive(true);
+            }
+
+            // Wait for enableDelay seconds
+            yield return new WaitForSeconds(enableDelay);
+
+            // Disable one child
+            children[disableIndex].gameObject.SetActive(false);
+            disableIndex = (disableIndex + 1) % children.Length;
+
+            // Move to the next set of children to enable
+            enableIndex = (enableIndex + childrenToEnable) % children.Length;
+
+            // Wait for disableDelay seconds before enabling the next set of children
+            yield return new WaitForSeconds(disableDelay - enableDelay);
+        }
+    }
+}
